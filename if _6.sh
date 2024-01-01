44e653d2176ac4bb512a075a0b61c7f0a6b73cdb
Fayyad <mohamedfayad080@gmail.com>
2024-01-01 22:27:22 +0200

    initial commit

---
+++ if _6.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# read -p "enter string: " str
+# if [ -z $str ]
+# then 
+# echo "this string is empty"
+# else
+# echo "this sting is not empty"
+# fi
+
+# read -p "enter number: " number 
+# if [ $number -lt 125 ]
+# then 
+# echo "Value is less than 125"
+# elif [ $number -lt 200 ]
+# then 
+# echo "the value is between 125 and 200"
+# else 
+# echo "value is greater than 200"
+# fi 
